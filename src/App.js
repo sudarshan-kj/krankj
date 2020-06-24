13103b4edcf6a404f570d1dbea7ee88e8710ba09
kjsudi <kjsudi@gmail.com>
2020-06-24 21:48:11 +0530

    Update icons and main page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} color="white" className="App-logo" alt="logo" />
         <p>
           <code>Site is currently under development. Please come back on July 15th, 2020</code>
         </p>
