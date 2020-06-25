6dc2565da48a9067c6023f8ad3d8bc24a223f1dd
kjsudi <kjsudi@gmail.com>
2020-06-25 15:54:58 +0530

    Handle click changes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,34 @@ import React from 'react';
 import logo from './gear.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+constructor(props){
+  super(props);
+  this.state = {bgColor: "#ff0055", colorTracker: -1};
+}
+
+toggleColors = () => {
+  var colors = ["#47fa00","#000000", "#00ddfa","#fac000","#ff0055"];
+  this.setState(prevState =>  {
+    var colorIndex = prevState.colorTracker + 1;
+    colorIndex = colorIndex % (colors.length );
+    return ( {colorTracker: colorIndex, bgColor: colors[this.state.colorTracker] }) }
+  );
+};
+
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header"  onClick={() => this.toggleColors()} style={{backgroundColor: this.state.bgColor}} >
+        <img src={logo} className="App-logo"  alt="logo" />
         <p>
           <b><code>Site is currently under development. Please come back on July 15th, 2020</code></b>
         </p>
       </header>
     </div>
   );
+}
 }
 
 export default App;
