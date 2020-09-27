bd78a284c92d2ae28e8b13e8163119771d117cdd
Sudarshan K J <kjsudi@gmail.com>
2020-09-27 18:42:32 +0530

    Added file loader

--- webpack.config.js
+++ webpack.config.js
@@ -54,10 +54,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-        loader: "url-loader",
-        options: {
-          limit: 8192,
-        },
+        use: [{ loader: "file-loader", options: {} }],
       },
     ],
   },
