f2f484e0d2f16a7c06f356a9aae7cea6ec1c107b
Sudarshan K J <kjsudi@gmail.com>
2020-09-27 18:53:26 +0530

    Added basic loading

--- src/components/pages/Gallery/index.js
+++ src/components/pages/Gallery/index.js
@@ -3,15 +3,37 @@ import styles from "./gallery.module.css";
 import myPhoto from "../../../assets/girl.jpg";
 
 const Gallery = () => {
+  const [imageLoading, setImageLoading] = React.useState(true);
+
+  const imageLoaded = () => {
+    setImageLoading(false);
+  };
+
+  const Image = () => {
+    return (
+      <>
+        <p style={imageLoading ? { display: "block" } : { display: "none" }}>
+          Loading...
+        </p>
+        <img
+          style={imageLoading ? { display: "none" } : { display: "inline" }}
+          src={myPhoto}
+          alt="myPhoto"
+          onLoad={imageLoaded}
+        />
+      </>
+    );
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.imageLayout}>
-        <img src={myPhoto} alt="myPhoto" />
-        <img src={myPhoto} alt="myPhoto" />
-        <img src={myPhoto} alt="myPhoto" />
-        <img src={myPhoto} alt="myPhoto" />
-        <img src={myPhoto} alt="myPhoto" />
-        <img src={myPhoto} alt="myPhoto" />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
       </div>
     </div>
   );
