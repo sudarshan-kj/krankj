67bb88de6f27ec98ee68bf96eaf360b92c7e3018
Sudarshan K J <kjsudi@gmail.com>
2020-09-28 18:12:24 +0530

    Added 3 column layout. Customized scroll bar

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,15 @@ import styled from "styled-components";
 const StyledDiv = styled.div`
   display: flex;
   align-items: center;
-  margin: 0rem 3rem;
+  margin: 0rem 1rem;
   font-size: 0.5rem;
   height: 10vh;
+
+  @media screen and (min-width: 960px) {
+    div {
+      margin: 0rem 3rem;
+    }
+  }
 `;
 
 const Footer = () => <StyledDiv>&copy; All Rights Reserved</StyledDiv>;
