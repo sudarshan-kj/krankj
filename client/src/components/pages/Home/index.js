d770ed2b3800924fa60efb20fae7fd68792a2469
Sudarshan K J <kjsudi@gmail.com>
2020-10-05 08:47:02 +0530

    Fix small animation glitch while loading home page. Added new images

--- client/src/components/pages/Home/index.js
+++ client/src/components/pages/Home/index.js
@@ -6,24 +6,26 @@ import myImage from "../../../assets/images/myimg.jpg";
 const HomePage = () => (
   <div className={styles.mainContainer}>
     <div className={styles.contentContainer}>
-      <section className={styles.introSection}>
-        <h1>Hello there 👋🏼</h1>
-        <p>
-          I'm Sudarshan KJ,
-          <br />
-          Web Developer and Designer
-        </p>
-        <Link className={styles.moreLink} to="/about">
-          {">>"}
-        </Link>
-      </section>
-      <section className={styles.profilePictureSection}>
-        <img
-          className={styles.profilePicutreImg}
-          src={myImage}
-          alt="sudarshan"
-        />
-      </section>
+      <div className={styles.animatedContainer}>
+        <section className={styles.introSection}>
+          <h1>Hello there 👋🏼</h1>
+          <p>
+            I'm Sudarshan KJ,
+            <br />
+            Web Developer and Designer
+          </p>
+          <Link className={styles.moreLink} to="/about">
+            {">>"}
+          </Link>
+        </section>
+        <section className={styles.profilePictureSection}>
+          <img
+            className={styles.profilePicutreImg}
+            src={myImage}
+            alt="sudarshan"
+          />
+        </section>
+      </div>
     </div>
   </div>
 );
