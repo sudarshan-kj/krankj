9027c8d6f7bc555d63b5a75f0d5d6c10a400765c
Sudarshan K J <kjsudi@gmail.com>
2020-10-06 22:17:50 +0530

    Installed mongodb and mongoose. Added code to accept user submissions

--- server.js
+++ server.js
@@ -3,7 +3,26 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 9000;
+const mongoose = require("mongoose");
 const rateLimit = require("express-rate-limit");
+
+let mongoDB = "mongodb://127.0.0.1/test";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  email: String,
+  name: String,
+  message: String,
+});
+
+let User = mongoose.model("User", userSchema);
+
+//Get the default connection
+let db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 const getApiLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 60 minutes
@@ -42,6 +61,20 @@ let msg = {
 
 app.post("/api/world", (req, res) => {
   res.send(msg);
+});
+
+app.post("/api/contact", function (request, response) {
+  let u = new User({
+    email: request.body.email,
+    name: request.body.name,
+    message: request.body.message,
+  });
+
+  u.save(function (err) {
+    if (err) throw err;
+    else console.log("Successfully saved user");
+  });
+  response.send({ msg: " ok" });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
