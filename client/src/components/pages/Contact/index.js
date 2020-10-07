89ca23a3bad42a79c53e0c6472a6fcb4a2f20c3c
Sudarshan K J <kjsudi@gmail.com>
2020-10-07 22:10:24 +0530

    Added formik and basic message validation

--- client/src/components/pages/Contact/index.js
+++ client/src/components/pages/Contact/index.js
@@ -1,29 +1,51 @@
 import React from "react";
 import styles from "./Contact.module.css";
+import { useFormik } from "formik";
+import axios from "axios";
+import { API_ENDPOINT } from "../../../constants";
 
 const Contact = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const postData = (values) => {
+    axios
+      .post(`${API_ENDPOINT}/api/contact/submit`, values)
+      .then((res) => console.log(res.data.msg))
+      .catch((err) =>
+        console.log("Error occurred while submittin the form data", err)
+      );
   };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      name: "",
+    },
+    onSubmit: (values, { resetForm }) => {
+      postData(values);
+      resetForm({ values: "" });
+    },
+  });
 
   return (
     <div className={styles.mainContainer}>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Email:
-          <input type="email" name="email" />
-        </label>
-        <label>
-          Message:
-          <textarea />
-        </label>
-        <button type="submit" value="Submit">
-          Submit
-        </button>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
