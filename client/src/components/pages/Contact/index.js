95d75989d822a0b5017324f0ae5587f939d9a693
Sudarshan K J <kjsudi@gmail.com>
2020-10-11 07:06:44 +0530

    Added message text area and styled it

--- client/src/components/pages/Contact/index.js
+++ client/src/components/pages/Contact/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import styles from "./Contact.module.css";
-import { useFormik } from "formik";
+import { useFormik, Field } from "formik";
 import * as Yup from 'yup';
 import axios from "axios";
 import { API_ENDPOINT } from "../../../constants";
 
 const MAX_NAME_LENGTH = 35;
 const MAX_EMAIL_LENGTH = 45;
+const MAX_MESSAGE_LENGTH = 300;
 
 const yupValidationObject = Yup.object({
   name: Yup.string()
     .max(MAX_NAME_LENGTH, 'Ah! That\'s too long! Try a bit shorter')
     .required('Please fill your name'),
   email:  Yup.string().max(MAX_EMAIL_LENGTH, 'That email is way too long.').email('That doesn\'t look like an email address').required('Please fill your email'),
+  message: Yup.string().max(MAX_MESSAGE_LENGTH,'You have exceeded your message limit').required('Atleast a short message is required')
 });
 
 const Contact = () => {
@@ -29,6 +31,7 @@ const Contact = () => {
     initialValues: {
       email: "",
       name: "",
+      message: ""
     }, validationSchema: yupValidationObject,
     onSubmit: (values, { resetForm }) => {
       postData(values);
@@ -74,6 +77,21 @@ const Contact = () => {
          <div className={styles.errorMsg}>{formik.errors.email}</div> 
        ) : null}
           </li>
+          <li>
+            <label htmlFor="message">Message</label>
+            <textarea 
+            name="message" 
+            id="message" 
+            value={formik.values.message} 
+            onChange={ e => {
+                if (e.target.value.length <= MAX_MESSAGE_LENGTH + 1)
+                formik.handleChange(e)
+              }}
+            onBlur={formik.handleBlur}/>
+          {formik.touched.message && formik.errors.message ? (
+         <div className={styles.errorMsg}>{formik.errors.message}</div> 
+       ) : null}
+                 </li>
           <li>
             <button type="submit">Submit</button>
           </li>
