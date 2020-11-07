40ebe5341d91e4d5f48fe007d321cf66a0dd91d8
Sudarshan K J <kjsudi@gmail.com>
2020-11-07 06:40:55 +0530

    Update mongoose

--- common/services/mongoose.service.js
+++ common/services/mongoose.service.js
@@ -3,8 +3,11 @@ const log4js = require("log4js");
 const logger = log4js.getLogger();
 logger.level = "debug";
 let count = 0;
-let mongoDBUrl = "mongodb://127.0.0.1/test";
 
+// let mongoDBUrl = "mongodb://127.0.0.1/test";
+const MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD;
+const MONGO_DB_USERNAME = process.env.MONGO_DB_USERNAME;
+let mongoDBUrl = `mongodb+srv://${MONGO_DB_USERNAME}:${MONGO_DB_PASSWORD}@cluster0.fpezm.mongodb.net/test?retryWrites=true&w=majority`;
 const options = {
   autoIndex: false, // Don't build indexes
   poolSize: 10, // Maintain up to 10 socket connections
