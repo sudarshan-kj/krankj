91e57b8d57323d1d8b8fd98f481102236b5ecce2
Sudarshan K J <kjsudi@gmail.com>
2020-11-10 20:55:04 +0530

    Updated api config to match vercel's requirements

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,2 +1,2 @@
-export const API_PORT = 9000;
-export const API_ENDPOINT = `http://192.168.0.199:${API_PORT}`;
+export const API_PORT = 443;
+export const API_ENDPOINT = `https://krankj-backend.krankj.vercel.app:${API_PORT}`;
