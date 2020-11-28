62efba421947d0d778980904255de266ba4277d1
Sudarshan K J <kjsudi@gmail.com>
2020-11-28 07:03:55 +0530

    Updtae db url

--- api/common/services/mongoose.service.js
+++ api/common/services/mongoose.service.js
@@ -7,9 +7,9 @@ let count = 0;
 // let mongoDBUrl = "mongodb://127.0.0.1/test";
 const MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD;
 const MONGO_DB_USERNAME = process.env.MONGO_DB_USERNAME;
-let mongoDBUrl =
-  "mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
-// let mongoDBUrl = `mongodb+srv://${MONGO_DB_USERNAME}:${MONGO_DB_PASSWORD}@cluster0.fpezm.mongodb.net/test?retryWrites=true&w=majority`;
+// let mongoDBUrl =
+//   "mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+let mongoDBUrl = `mongodb+srv://${MONGO_DB_USERNAME}:${MONGO_DB_PASSWORD}@cluster0.fpezm.mongodb.net/test?retryWrites=true&w=majority`;
 const options = {
   autoIndex: false, // Don't build indexes
   poolSize: 10, // Maintain up to 10 socket connections
